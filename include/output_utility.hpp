74d9658d6a54b8bfa793e03810456a66808ce529
Pachicobue <tigerssho@gmail.com>
2020-08-09 03:54:03 +0900

    util系を追加

---
+++ include/output_utility.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+template<typename T, typename A>
+inline std::ostream& operator<<(std::ostream& os, const std::vector<T, A>& v)
+{
+    os << "[";
+    for (const auto& e : v) { os << e << ","; }
+    return (os << "]");
+}
+template<typename T, typename C, typename A>
+inline std::ostream& operator<<(std::ostream& os, const std::set<T, C, A>& v)
+{
+    os << "[";
+    for (const auto& e : v) { os << e << ","; }
+    return (os << "]");
+}
+inline std::ostream& operator<<(std::ostream& os, const std::byte v)
+{
+    constexpr const char* chars = "0123456789ABCDEF";
+    return (os << "0x" << chars[static_cast<int>(v) >> 4] << chars[static_cast<int>(v) & (0x0F)]);
+}
+template<typename T>
+inline std::string hex_str(T v)
+{
+    constexpr const char* chars = "0123456789ABCDEF";
+    std::string str;
+    for (; v > 0; v >>= 4) { str.push_back(chars[v & 0x0F]); }
+    std::reverse(str.begin(), str.end());
+    return ("0x" + str);
+}
